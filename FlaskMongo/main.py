d67d75a492503b5fd9edb242e5dd30658359ad1d
diego-debian <profediegoparra01@gmail.com>
2022-10-30 11:23:07 -0500

    Servidor corriendo

--- FlaskMongo/main.py
+++ FlaskMongo/main.py
@@ -1,4 +1,18 @@
 from flask import Flask, request, Response
-from flask impor jsonify
+from flask import jsonify
 from flask_cors import CORS
 
+app = Flask(__name__)
+cors = CORS(app)
+
+####################################
+##    PROBAR EL SERVICIO          ##
+####################################
+@app.route("/", methods=["GET"])
+def test():
+    json = {}
+    json["message"] = "Server running ..."
+    return jsonify(json)
+
+if __name__ == "__main__":
+    app.run(debug=False, port=9000)
