c3871c02b9a22f564bc49b50dfd4cd26b23bb805
diego-debian <profediegoparra01@gmail.com>
2022-11-27 18:21:37 -0500

    Frontend terminado y funcionando

--- Frontend/src/app/modelos/mesas.model.ts
+++
@@ -1,5 +0,0 @@
-export class Mesas {
-  _id?:string;
-  numero?:string;
-  cantidad_inscritos?:string;
-}
--- Frontend/src/app/modelos/usuario.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Usuario } from './usuario.model';
-
-describe('Usuario', () => {
-  it('should create an instance', () => {
-    expect(new Usuario()).toBeTruthy();
-  });
-});
